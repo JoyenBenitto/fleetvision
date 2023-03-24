6503e40f3263e00433ac9edd72e5e9799102b465
suhaskv1 <suhaskv0111@gmail.com>
2023-03-24 11:20:29 +0530

    driver code cleanup and comments

--- drivers/fvrpi/src/main.rs
+++ drivers/fvrpi/src/main.rs
@@ -4,6 +4,7 @@ use std::thread;
 use std::time::Duration;
 
 fn read_sensor(sensor: Arc<Mutex<InputPin>>) {
+    // Function to read sensor data at real-time
     loop {
         if sensor.lock().unwrap().read() == Level::High {
             println!("Obstacle detected on sensor {:?}", sensor);
@@ -15,23 +16,24 @@ fn read_sensor(sensor: Arc<Mutex<InputPin>>) {
 }
 
 fn main() {
+    // Main function to run sensor mutex op
     let gpio = Gpio::new().unwrap();
 
-    let sensor1_pin = gpio.get(17).unwrap().into_input();
-    let sensor2_pin = gpio.get(18).unwrap().into_input();
+    let ir_pin = gpio.get(17).unwrap().into_input(); // IR sensor pin
+    let temp_pin = gpio.get(18).unwrap().into_input(); // Temperature sensor pin
 
-    let sensor1 = Arc::new(Mutex::new(sensor1_pin));
-    let sensor2 = Arc::new(Mutex::new(sensor2_pin));
+    let ir = Arc::new(Mutex::new(ir_pin));
+    let temp = Arc::new(Mutex::new(temp_pin));
 
-    let sensor1_thread = thread::spawn(move || {
-        read_sensor(sensor1);
+    let ir_thread = thread::spawn(move || {
+        read_sensor(ir);
     });
 
-    let sensor2_thread = thread::spawn(move || {
-        read_sensor(sensor2);
+    let temp_thread = thread::spawn(move || {
+        read_sensor(temp);
     });
 
-    sensor1_thread.join().unwrap();
-    sensor2_thread.join().unwrap();
+    ir_thread.join().unwrap();
+    temp_thread.join().unwrap();
 }
 

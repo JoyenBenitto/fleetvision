29219f404c9bc748c54b8e593fc780fa3495501c
suhaskv1 <suhaskv0111@gmail.com>
2023-03-24 11:08:20 +0530

    Revert "partial rust driver code added"
    This reverts commit 5fd3a28e13f0fd5297a8d1feb74dc356d194ab5a.

--- drivers/fvrpi/target/debug/deps/lazy_static-b3b6802f1d8a5a12.d
+++
@@ -1,8 +0,0 @@
-/home/suhas/Documents/fleetvision/drivers/fvrpi/target/debug/deps/lazy_static-b3b6802f1d8a5a12.rmeta: /home/suhas/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs /home/suhas/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs
-
-/home/suhas/Documents/fleetvision/drivers/fvrpi/target/debug/deps/liblazy_static-b3b6802f1d8a5a12.rlib: /home/suhas/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs /home/suhas/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs
-
-/home/suhas/Documents/fleetvision/drivers/fvrpi/target/debug/deps/lazy_static-b3b6802f1d8a5a12.d: /home/suhas/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs /home/suhas/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs
-
-/home/suhas/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs:
-/home/suhas/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs:
--- drivers/fvrpi/target/debug/fvrpi.d
+++
@@ -1,1 +0,0 @@
-/home/suhas/Documents/fleetvision/drivers/fvrpi/target/debug/fvrpi: /home/suhas/Documents/fleetvision/drivers/fvrpi/src/main.rs

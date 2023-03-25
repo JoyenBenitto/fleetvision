f01a2df755025b707612490275925164a911f79b
suhaskv1 <suhaskv0111@gmail.com>
2023-03-25 22:00:38 +0530

    fvpy parse

--- drivers/fvpy/log.py
+++ drivers/fvpy/log.py
@@ -3,8 +3,11 @@ import serial
 import string
 import time
 
-serial_port = serial.Serial('/dev/ttyUSB2', 9600)
+serial_port = serial.Serial('/dev/ttyACM0', 9600)
 
 while True:
     serial_data = serial_port.readline()
+    serial_data = serial_data.split(',')
+    serial_data[0] = serial_data[0][2:]
+    serial_data[2] = serial_data[2][:-2] 
     print(serial_data)

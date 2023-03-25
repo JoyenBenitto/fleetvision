91053461731cd71e2eb1ba60206c493e4c141993
suhaskv1 <suhaskv0111@gmail.com>
2023-03-26 01:34:25 +0530

    added fixed logger

--- drivers/fvpy/log.py
+++ drivers/fvpy/log.py
@@ -2,13 +2,23 @@ import logging
 import serial
 import string
 import time
-import subprocess
+import sys
+import os
 
 serial_port = serial.Serial('/dev/ttyACM0', 9600)
+o_stdout = sys.stdout
+if os.path.exists("log.csv"):
+    os.remove("log.csv")
 
 while True:
-    serial_data = str(serial_port.readline())
-    serial_data = serial_data.split(',')
-    serial_data[0] = serial_data[0][2:]
-    serial_data[2] = serial_data[2][:-2] 
+    serial_data = serial_port.readline()
+    serial_data = serial_data.decode()
+    serial_data = serial_data.rstrip() 
     print(serial_data)
+    file = open('log.csv', 'a+')
+    file.write(serial_data)
+    file.write('\n')
+    file.close()
+    time.sleep(0.1)
+
+serial_data.close()
--- drivers/fvpy/writer.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-file_ = open('log.txt', 'w+')
-subprocess.run('python3 log.py', shell=True, stdout=file_)
-file_.close()

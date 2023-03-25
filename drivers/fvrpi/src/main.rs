b2a4b77999309b31f095b599e35246e765a14f2d
suhaskv1 <suhaskv0111@gmail.com>
2023-03-25 18:16:12 +0530

    fixed temp sensor driver

--- drivers/fvrpi/src/main.rs
+++ drivers/fvrpi/src/main.rs
@@ -9,9 +9,7 @@ pub mod utils;
 
 const IR_SENSOR_PIN: u8 = 17;
 const I2C_DEV_ADDRESS: u16 = 0x5A;
-const AMB_TEMP_REG: u8 = 0x06;
-const OBJ_TEMP_REG: u8 = 0x07;
-const SENSOR_PARAMETERS_NO: usize = 3;
+const SENSOR_PARAMETERS_NO: usize = 2;
 
 fn read_prox_sensor(pin: &InputPin) -> f32 {
     // Function to read IR sensor
@@ -36,22 +34,22 @@ fn display_sensor_data() {
     let _ir_pin = gpio.get(IR_SENSOR_PIN).unwrap().into_input_pullup();
 
     let mut inc = 1;
-    let mut sensor_data_arr: [f32; SENSOR_PARAMETERS_NO] = [0.0; 3];
-    let mut sensor_parameters_arr: [f32; SENSOR_PARAMETERS_NO] = [0.0; 3];
+    let mut sensor_data_arr: [f32; SENSOR_PARAMETERS_NO] = [0.0; 2];
+    let mut sensor_parameters_arr: [f32; SENSOR_PARAMETERS_NO] = [0.0; 2];
 
     utils::print_type_of(&sensor_data_arr);
 
     loop {
-        let ambient_temp = read_temperature(&mut i2c, AMB_TEMP_REG);
-        let object_temp = read_temperature(&mut i2c, OBJ_TEMP_REG);
+        let ambient_temp = read_temperature(&mut i2c);
+        //let object_temp = read_temperature(&mut i2c, OBJ_TEMP_REG);
         let ir_distance = read_prox_sensor(&_ir_pin);
 
-        sensor_parameters_arr = [ambient_temp, object_temp, ir_distance];
+        sensor_parameters_arr = [ambient_temp, ir_distance];
 
         println!("{:?}", inc);
         inc += 1;
         println!("Ambient temperature: {:.2}C", ambient_temp);
-        println!("Object temperature: {:.2}C", object_temp);
+        //println!("Object temperature: {:.2}C", object_temp);
         println!("IR Intrusion: {:.2}", ir_distance);
 
         for x in 0..SENSOR_PARAMETERS_NO {
@@ -64,29 +62,21 @@ fn display_sensor_data() {
     }
 }
 
-fn read_temperature(i2c: &mut I2c, register: u8) -> f32 {
+fn read_temperature(i2c: &mut I2c) -> f32 {
     // Function to read and parse temperature
-    let mut buffer = [0u8; 3];
-    i2c.write(&[register]).unwrap();
-    thread::sleep(Duration::from_micros(500));
-    i2c.read(&mut buffer).unwrap();
-    let temperature_data = u16::from_be_bytes([buffer[0], buffer[1]]) >> 2;
-    let temperature = (temperature_data as f32) * 0.02 - 273.15;
-    temperature
+    i2c.write(&[0x07]).unwrap();
+    thread::sleep(Duration::from_millis(500));
+
+    let mut buf = [0u8; 2];
+    i2c.read(&mut buf).unwrap();
+
+    let temp_raw = (buf[1] as u16) << 8 | buf[0] as u16;
+    let temp_c = ((temp_raw as f32) * 0.02 - 273.15) as f32;
+    temp_c
 }
 
 fn main() {
     // Main function to run stuff
 
-    let log = OpenOptions::new()
-        .truncate(true)
-        .read(true)
-        .create(true)
-        .write(true)
-        .open(utils::get_temp_filepath())
-        .unwrap();
-
-    utils::print_type_of(&log);
-    //utils::log_sensor_data(log);
     display_sensor_data();
 }
--- drivers/fvrpi/src/utils.rs
+++ drivers/fvrpi/src/utils.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::Write;
 
-const SENSOR_PARAMETERS_NO: usize = 3;
+const SENSOR_PARAMETERS_NO: usize = 2;
 
 pub fn get_temp_filepath() -> String {
     //Function to return filepath for temp log
@@ -10,14 +10,12 @@ pub fn get_temp_filepath() -> String {
     return "/tmp/mylog.log".into();
 }
 
-pub fn log_sensor_data(sensor_array: [f32; SENSOR_PARAMETERS_NO]) {
-    let temp_directory = env::temp_dir();
-    let temp_file = temp_directory.join("mylog.log");
-
-    let mut file = File::create(temp_file).unwrap();
-    writeln!(&mut file, "{:?}", sensor_array).unwrap();
-    file.write(b"Bytes\n").unwrap();
-}
+/*pub fn log_sensor_data(sensor_array: [f32; SENSOR_PARAMETERS_NO]) {
+    let mut file_ref = std::fs::File::create("mylog.log").expect("create failed");
+    file_ref
+        .write_all(&sensor_array[0..(SENSOR_PARAMETERS_NO - 1)].as_str())
+        .expect("write failed");
+}*/
 
 pub fn print_type_of<T>(_: &T) {
     println!("{}", std::any::type_name::<T>())
